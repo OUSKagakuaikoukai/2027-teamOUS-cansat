1c67dca458ab41cab39bed849e066ac6592eefb7
kenkohoshi <hitsujikun30@gmail.com>
2026-06-24 18:51:34 +0900

    main.cppでモータ関数を動かす

--- cansat_software/src/main.cpp
+++ cansat_software/src/main.cpp
@@ -1,18 +1,11 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
+#include<motor.h>
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  MotorSetUp(5,18,0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Move(0,255);
 }

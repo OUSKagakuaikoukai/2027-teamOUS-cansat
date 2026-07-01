d6c8b39c01317cccc1cfa218bab6347ec62925e3
kenkohoshi <hitsujikun30@gmail.com>
2026-07-02 01:04:24 +0900

    前進するプログラムです

--- cansat_software/src/main.cpp
+++ cansat_software/src/main.cpp
@@ -3,9 +3,11 @@
 void setup() {
   // put your setup code here, to run once:
   MotorSetUp(5,18,0);
+  MotorSetUp(26,25,1);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Move(0,255);
+  Move(0,200);
+  Move(1,200);
 }

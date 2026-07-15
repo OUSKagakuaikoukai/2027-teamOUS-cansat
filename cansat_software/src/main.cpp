4b0323bf5a3383e1514d6a3a04be08077e0660b2
kenkohoshi <hitsujikun30@gmail.com>
2026-07-15 18:52:25 +0900

    サーボモータ動作確認をしました

--- cansat_software/src/main.cpp
+++ cansat_software/src/main.cpp
@@ -1,13 +1,37 @@
 #include <Arduino.h>
 #include<motor.h>
+#include <ESP32Servo.h>
+
+Servo servo;
+
+#define SERVO_PIN 17
+
 void setup() {
-  // put your setup code here, to run once:
+  /*
   MotorSetUp(5,18,0);
   MotorSetUp(26,25,1);
+  */
+
+servo.setPeriodHertz(50);
+
+// サーボを接続
+// 最小パルス幅500us、最大2500us（一般的な値）
+servo.attach(SERVO_PIN, 500, 2500);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  /*
   Move(0,200);
   Move(1,200);
+  */
+
+  // 0°
+    servo.write(0);
+    delay(1000);
+
+    // 45°
+    servo.write(45);
+    delay(1000);
+
 }

9290cfe71e621a910416444a6bc5be6914edabe0
kenkohoshi <hitsujikun30@gmail.com>
2026-07-24 18:34:29 +0900

    コードを更新しました

--- cansat_software/src/main.cpp
+++ cansat_software/src/main.cpp
@@ -2,14 +2,15 @@
 #include<motor.h>
 #include <ESP32Servo.h>
 
-Servo servo;
+Servo servo0,servo1;
 
-#define SERVO_PIN 17
+#define SERVO_PIN0 32
+#define SERVO_PIN1 33
 
 #define MOTOR0_PIN1 17
 #define MOTOR0_PIN2 18
-#define MOTOR1_PIN1 26
-#define MOTOR1_PIN2 25
+#define MOTOR1_PIN1 25
+#define MOTOR1_PIN2 26
 
 
 
@@ -19,11 +20,13 @@ void setup() {
   MotorSetUp(MOTOR1_PIN1,MOTOR1_PIN2,1);
   
 
-servo.setPeriodHertz(50);
+  servo0.setPeriodHertz(50);
+  servo1.setPeriodHertz(50);
 
 // サーボを接続
 // 最小パルス幅500us、最大2500us（一般的な値）
-servo.attach(SERVO_PIN, 500, 2500);
+  servo0.attach(SERVO_PIN0, 500, 2500);
+  servo1.attach(SERVO_PIN1, 500, 2500);
 
 }
 
@@ -34,11 +37,13 @@ void loop() {
   
 
   // 0°
-    //servo.write(0);
+    servo0.write(0);
+	servo1.write(0);
     delay(1000);
 
     // 45°
-    //servo.write(45);
+    servo0.write(45);
+	servo1.write(45);
     delay(1000);
 
 }
